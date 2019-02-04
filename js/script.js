0fdf4fc24de55ad61a66bef844b6577e3dd9fc0c
Irwing <ivielma101@gmail.com>
2019-02-04 23:07:17 +0000

    erased console log

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 
 // BELOW Update the songs array with four of your favorites songs.
 var songs = ["Swervin","Sad","Sorry not sorry", "Dont"];
-// BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 var artists = ["Aboogie wit da hoodie","xxxtentacion","Bryson Tiller","Bryson Tiller"];
 var songlengths= ["3:08","2:47","3:23","3:43"];
 var images_links=["https://i.ytimg.com/vi/v-rxFHRTrJc/maxresdefault.jpg","https://i.ytimg.com/vi/pgN-vvVVxMA/maxresdefault.jpg","https://static.qobuz.com/images/covers/93/72/0886445447293_600.jpg","https://static.qobuz.com/images/covers/93/72/0886445447293_600.jpg"];
@@ -26,9 +25,7 @@ function displaySongInfo(){
     images_links.forEach(function(image){
         $("#images").append("<img src='"+image+"'>");
     });
-    console.log(videolink);
     videolink.forEach(function(links){
-        console.log(links);
         $("#links").append("<a href="+links+">click here to listen</a>");
     });
 };
@@ -69,4 +66,3 @@ $("#add").click(function() {
 });
 
 displaySongInfo();
-console.log("Loaded");
